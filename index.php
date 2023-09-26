9d42c2d20b7493bbe9922eb11d2262195f7a8bce
Mrlion <mrlion4202@gmail.com>
2023-09-26 15:43:56 +0200

    Files
    Uploaded new files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>🦁 Lion's Project</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<h2>🦁 Lion's Project</h2>
+
+</body>
+</html>
