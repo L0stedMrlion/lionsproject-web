dd65a091ec6cdfafc472be6afa27f29d97c2938a
Mrlion <mrlion4202@gmail.com>
2023-10-24 17:56:41 +0000

    added new files, complete rework

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 - My first web, I ever created
 - Web from my project named [🦁 Lion's Project](https://discord.gg/sWvj5M6e7W)
-
--- database.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$db_host = 'localhost'; #TODO Edit this!
-$db_user = 'root'; #TODO Edit this!
-$db_password = ''; #TODO Edit this!
