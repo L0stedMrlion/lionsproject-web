38dd91e79ed90aa46b9dcb769e70cec3100eea2b
Mrlion <mrlion4202@gmail.com>
2023-11-14 14:34:20 +0000

    deleted dark mode, bcs not working icons

--- scripts/darkmode.js
+++
@@ -1,6 +0,0 @@
-var isDarkMode = localStorage.getItem("isDarkMode") === "true";
-var toggleDarkMode = function () {
-    isDarkMode = !isDarkMode;
-    localStorage.setItem("isDarkMode", isDarkMode.toString());
-    document.documentElement.style.setProperty("color-scheme", isDarkMode ? "dark" : "light");
-};
