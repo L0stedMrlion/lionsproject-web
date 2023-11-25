3a9f6af2675e09438fba3523806cf81d406a1eae
Mrlion <mrlion4202@gmail.com>
2023-11-25 19:11:15 +0100

    app.vue update

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,11 @@
     </div>
 
     <div class="right">
-      <a href="http://github.com/L0stedMrlion" target="_blank" rel="noopener noreferrer">
-        <span>Github</span>
-      </a>
+      <a href="http://github.com/L0stedMrlion" target="_blank" rel="noopener noreferrer"> </a>
+      <i class="fa-brands fa-instagram"></i>
 
-      <a href="http://discord.gg/qMVEyW2Teq" target="_blank" rel="noopener noreferrer">
-        <span>Discord</span>
-      </a>
-
-      <a href="mailto:mrlion@lionsproject.eu">
-        <span>Email</span>
-      </a>
+      <a href="http://discord.gg/qMVEyW2Teq" target="_blank" rel="noopener noreferrer"> </a>
+      <i class="fa-brands fa-discord"></i>
     </div>
   </nav>
 </template>
