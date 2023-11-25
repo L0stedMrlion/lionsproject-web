7234f56ccf6edf3bbbb931bffc7496e609a1cd0a
Mrlion <mrlion4202@gmail.com>
2023-11-25 12:19:27 +0100

    deleted logo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
+  <header></header>
 </template>
 
 <style scoped>
